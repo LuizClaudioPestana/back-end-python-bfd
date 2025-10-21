dd57a8563c5ad6d128468c72438b32b1efee5f99
Luiz Cláudio Pestana <luiz.cp14@gmail.com>
2025-10-21 23:30:07 +0000

    ajustes realizados - db funcionando

--- aula_bd/aula_02.sql
+++ aula_bd/aula_02.sql
@@ -1,1 +1,15 @@
+.tables
+.schema Aluno
 select * from Aluno;
+
+
+
+select *
+from Aluno
+inner join Turma
+on id_turma = Turma.id;
+
+select *
+from Aluno
+right join Turma
+on id_aluno = Aluno.id;
